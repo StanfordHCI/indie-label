9d6476adc4abe1cc99506b9456611fda0ba532a8
Carol Anderson <carolmyrickanderson@gmail.com>
2023-10-24 22:14:50 -0600

    modify order of ops in run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python3 server.py > pylog.txt 2>&1 &
 cd indie_label_svelte
-host=0.0.0.0 port=5000 npm run dev 2>&1 &
+host=0.0.0.0 port=5000 npm run dev &
+cd ..
+python3 server.py
+
 
 
 
